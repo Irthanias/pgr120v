03a1baf3d5fc057743dad451250c1fb333cb9506
Irthanias <francisca.moe@gmail.com>
2025-08-25 18:09:23 +0200

    converter

---
+++ converter/converter.php
@@ -0,0 +1,20 @@
+<?php    /* Eksempel 1 */
+/*
+/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
+/*    Programmet skriver ut en "god dag"-melding med personens navn 
+*/
+  $NOK=$_POST ["NOK"];
+  $CLP=$_POST ["CLP"];
+
+  $kurs=95.41;
+  if($NOK){
+    $CLPr=$NOK*$kurs;
+    print("$NOK NOK er lik $CLPr CLP") ;
+  }
+  if($CLP){
+    $NOKr=$CLP/$kurs;
+    print("$CLP CLP er lik $NOKr NOK") ;
+  }
+	
+  
+?>
