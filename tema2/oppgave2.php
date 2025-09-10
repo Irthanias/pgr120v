9ba61f961aa5bcc441cc44c792f5877ec3eed248
Irthanias <francisca.moe@gmail.com>
2025-09-10 13:37:42 +0200

    opp2

---
+++ tema2/oppgave2.php
@@ -0,0 +1,22 @@
+<?php
+
+$svar=$_POST ["svar"];
+
+if ($svar == "ja")
+{
+    print("$svar, du er student");
+}
+elseif ($svar == "nei")
+{
+    print("$svar, du er ikke student");
+}
+else
+{
+    print("Skriv gyldig svar")
+}
+
+
+
+
+
+?>
