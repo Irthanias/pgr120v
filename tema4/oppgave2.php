c1a65ee90f9acaac17da79f272b3bfa638c24d67
Irthanias <francisca.moe@gmail.com>
2025-09-19 13:38:16 +0200

    ble

--- tema4/oppgave2.php
+++ tema4/oppgave2.php
@@ -2,24 +2,44 @@
 
 $klassekode=$_POST ["klassekode"];
 
-if (!$postnr)
-{
-     print("Postnr er ikke fylt ut");
-}
 
-if (strlen($postnr)!=4)
-{
-    print("Postnr best&aring;r ikke av 4 tegn");
-}
+  if (!$klassekode)  /* klassekode er ikke fylt ut */
+    {
+      print("Klassekode er ikke fylt ut <br />");
+    }
+  else if (strlen($klassekode)!=3)  /* klassekode består ikke av 3 tegn */
+    {
+      
+      print("Klassekode best&aring;r ikke av 3 tegn <br />");
+    }
+  else
+    {
+      $tegn1=$klassekode[0];   /* første tegn i klassekoden  */
+      $tegn2=$klassekode[1];   /* andre tegn i klassekoden  */
+      $tegn3=$klassekode[2];   /* tredje tegn i klassekoden  */
 
-if (!ctype_digit($postnr))
-{
-    print("Postnr m&aring; bare være tall");
-}
+      if (!ctype_alpha($tegn1))  /* tegn1 er ikke bokstav */ 
+        {
+          
+          print("F&oslash;rste tegn er ikke en bokstav <br />");
+        }
+		
+      if (!ctype_alpha($tegn2))  /* tegn2 er ikke bokstav */
+        {
+          
+          print("Andre tegn er ikke en bokstav <br />");
+        }
+		
+      if (!ctype_digit($tegn3))  /* tegn3 er ikke et siffer */ 
+        {
+          $lovligKlassekode=false;
+          print("Siste tegn er ikke et siffer  <br />");
+        }
+    }
 
-else
-{
-    print("Postnr er korrekt");
-}
+  else
+    {
+      print("Klassekode er korrekt fylt ut <br />");
+    }
 
 ?>
