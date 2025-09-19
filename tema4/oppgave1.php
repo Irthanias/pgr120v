810ef056fa8f62b953c2a85ba909020a72cbf864
Irthanias <francisca.moe@gmail.com>
2025-09-19 12:44:59 +0200

    oppgave1

---
+++ tema4/oppgave1.php
@@ -0,0 +1,25 @@
+<?php    
+
+$postnr=$_POST ["postnr"]
+
+if (!$postnr)
+{
+     print("Postnr er ikke fylt ut");
+}
+
+if (strlen($postnr)!=4)
+{
+    print("Postnr best&aring;r ikke av 4 tegn");
+}
+
+if (!ctype_digit($postnr))
+{
+    print("Postnr ms&aring; være tall");
+}
+
+else
+{
+    print("Postnr er korrekt")
+}
+
+?>
