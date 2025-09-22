fae0407f0638273bf78af7345250fd6f515b88ec
Irthanias <francisca.moe@gmail.com>
2025-09-22 19:42:27 +0200

    studreg

---
+++ Obligatorisk/studentReg.php
@@ -0,0 +1,51 @@
+<?php  /* registrer-poststed */
+/*
+/*  Programmet lager et html-skjema for å registrere et poststed
+/*  Programmet registrerer data (postnr og poststed) i databasen
+*/
+?> 
+
+<h3>Registrer Student </h3>
+
+<form method="post" action="" id="registrerStudentSkjema" name="registrerStudentSkjema">
+  Brukernavn <input type="text" id="brukernavn" name="brukernavn" required /> <br/>
+  Fornavn <input type="text" id="fornavn" name="fornavn" required /> <br/>
+  Etternavn <input type="text" id="etternavn" name="etternavn" required /> <br/>
+  <input type="submit" value="Registrer student" id="registrerStudentKnapp" name="registrerStudentKnapp" /> 
+  <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
+</form>
+
+<?php 
+  if (isset($_POST ["registrerstudentKnapp"]))
+    {
+      $brukernavn=$_POST ["brukernavn"];
+      $fornavn=$_POST ["fornavn"];
+      $etternavn=$_POST ["etternavn"];
+
+      if (!$brukernavn || !$fornavn || $etternavn ) 
+        {
+          print ("Brukernavn, fornavn og etternavn kode m&aring; fylles ut");
+        }
+      else
+        {
+          include("db-kobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
+
+          $sqlSetning="SELECT * FROM student WHERE brukernavn='$brukernavn';";
+          $sqlResultat=mysqli_query($db,$sqlSetning) or die ("Det er ikke mulig &aring; hente data fra databasen");
+          $antallRader=mysqli_num_rows($sqlResultat); 
+
+          if ($antallRader!=0)  /* data er registrert fra før */
+            {
+              print ("Data er registrert fra f&oslashr");
+            }
+          else
+            {
+              $sqlSetning="INSERT INTO klasse VALUES('$klasseKode','$klasseNavn','$studiumKode');";
+              mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; registrere data i databasen");
+                /* SQL-setning sendt til database-serveren */
+
+              print ("F&oslash;lgende data er n&aring; registrert: $brukernavn, $fornavn og $etternavn"); 
+            }
+        }
+    }
+?> 
