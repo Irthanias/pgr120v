0b13b047755cd110bab61ae618b598e13acf2d96
Irthanias <francisca.moe@gmail.com>
2025-09-22 14:54:26 +0200

    fg

---
+++ Obligatorisk/klasseHente.php
@@ -0,0 +1,27 @@
+<?php
+
+include("db-kobling.php");
+
+$sqlSetning="SELECT * FROM klasse;";
+
+$sqlResultat=mysqli_query($db,$sqlSetning) or die ("Det er ikke mulig &aring; hente data fra databasen");
+
+$antallRader=mysqli_num_rows($sqlResultat);
+
+print ("<h3>Registrerte klasser</h3>");
+print ("<table border=1>");
+print ("<tr><th align=left>Klassekode</th> <th align=left>Klassenavn</th> <th align=left>Studiekode</th></tr>");
+
+for ($r=1;$r<=$antallRader;$r++)
+{
+    $rad=mysqli_fetch_array($sqlResultat);
+    $klasseKode=$rad["klasseKode"];
+    $klasseNavn=$rad["klasseNavn"];
+    $studieKode=$rad["studieKode"];
+
+    print ("<tr> <td> $klasseKode </td> <td> $klasseNavn </td> <td> $studieKode </td> </tr>");
+}
+
+print ("</table>");
+
+?>
